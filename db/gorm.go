466a364bac8c3b9cea5c25b3cc6a0759e4de1882
duanchuansong <duanchuansong@sunmi.com>
2021-09-27 14:57:36 +0800

    feat:modify gorm

--- db/gorm.go
+++ db/gorm.go
@@ -16,14 +16,14 @@ type Mysql struct {
 }
 
 func NewDb(cfg *Mysql) *gorm.DB {
-	dsn := cfg.User + ":" + cfg.Pass + "@tcp(" + cfg.Host + ":"+cfg.Port+")/" + cfg.DbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := cfg.User + ":" + cfg.Pass + "@tcp(" + cfg.Host + cfg.Port + ")/" + cfg.DbName + "?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("open database err,err:"+err.Error())
+		panic("open database err,err:" + err.Error())
 	}
 	sqlDB, err := db.DB()
 	if err != nil {
-		panic("open database err,err:"+err.Error())
+		panic("open database err,err:" + err.Error())
 	}
 	sqlDB.SetMaxIdleConns(cfg.MaxIdleConns)
 	sqlDB.SetMaxOpenConns(cfg.MaxIdleConns)
